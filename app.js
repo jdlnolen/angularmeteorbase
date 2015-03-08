487d3e85d932b0cb424a96ee6d66e3679364cde5
John David Nolen <jdln@bishop.attlocal.net>
2015-03-08 15:32:53 -0500

    added a list controller

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 if (Meteor.isClient) {
-  angular.module('handshake',['angular-meteor']);
+  angular.module('angularbase',['angular-meteor']);
+ 
+  angular.module("angularbase").controller("ItemsListCtrl", ['$scope',
+      function($scope){
+
+        $scope.items = [
+          {'name': 'item 1',
+            'description': 'this is item 1'},
+          {'name': 'item 2',
+            'description': 'this is item 2'},
+          {'name': 'item 3',
+            'description': 'this is item 3'}
+        ];
+
+     }]);   
+
 }
+
