4bc52f6e8f9ac276854a47fc6eb62aa2365921a5
John David Nolen <jdln@bishop.attlocal.net>
2015-03-09 23:15:57 -0500

    step 11 done

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,6 +1,13 @@
+angular.module('angularbase',['angular-meteor', 'ui.router']);
 
-    angular.module('angularbase',['angular-meteor', 'ui.router']);
-    
+function onReady() {
+  angular.bootstrap(document, ['angularbase']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
    
     
     
