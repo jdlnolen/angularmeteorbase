4fc6e5164632ebaa2c175be87bcc2ba01d092da7
John David Nolen <jdln@bishop.local>
2015-03-09 20:39:00 -0500

    step 8 finished

--- model/items.js
+++ model/items.js
@@ -1,1 +1,19 @@
 Items = new Mongo.Collection("items");
+
+Items.allow({
+  insert: function (userId, item) {
+    return userId && item.owner === userId;
+  },
+  update: function (userId, item, fields, modifier) {
+    if (userId !== item.owner)
+      return false;
+
+    return true;
+  },
+  remove: function (userId, item) {
+    if (userId !== item.owner)
+      return false;
+
+    return true;
+  }
+});
