f2ae5f78239f040da1fc615dac0f44cea9b9e0a5
John David Nolen <jdln@bishop.attlocal.net>
2015-03-09 19:31:18 -0500

    finished step 6

--- app.js
+++ app.js
@@ -37,13 +37,34 @@ if (Meteor.isClient) {
 
     }]);
     
-    angular.module('angularbase').controller("ItemDetailsCtrl", ['$scope', '$stateParams',
-  function($scope, $stateParams){
+    angular.module('angularbase').controller("ItemDetailsCtrl", ['$scope', '$stateParams', '$meteor',
+  function($scope, $stateParams, $meteor){
 
-    $scope.itemId = $stateParams.itemId;
+    $scope.item = $meteor.object(Items, $stateParams.itemId, false);
+
+    $scope.save = function() {
+      $scope.item.save().then(function(numberOfDocs){
+        console.log('save success doc affected ', numberOfDocs);
+      }, function(error){
+        console.log('save error', error);
+      });
+    };
+
+    $scope.reset = function() {
+      $scope.item.reset();
+    };
 
 }]);
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 if (Meteor.isServer) {
